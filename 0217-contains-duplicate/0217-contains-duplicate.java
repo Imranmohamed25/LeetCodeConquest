d9d5cb91bc78c13155e694027b8c0ed8e1a43ab9
Imranmohamed M <123489831+Imranmohamed25@users.noreply.github.com>
2024-08-18 17:12:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 1 ; i++){
-            if(nums[i] == nums[i + 1]){
-                return true;
+        //HashSet hs = new HashSet<>();
+        boolean result = false;
+        for(int i = 0; i < nums.length ; i++){
+            for(int j = i + 1 ; j < nums.length ; j++){
+                if(i != j){
+                 if(nums[i] == nums[j]){
+                 result = true;
+                 break;
+                }
             }
         }
-        return false;
+    }
+     return result;
     }
 }
