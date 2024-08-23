f536fb46ae7f0c06c96fe184e9e61da643d42162
Imranmohamed M <123489831+Imranmohamed25@users.noreply.github.com>
2024-08-24 00:10:49 +0530

    Time: 6 ms (6.94%), Space: 42.1 MB (5.23%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,6 +1,19 @@
 class Solution {
     public String toLowerCase(String s) {
-        s = s.toLowerCase();
-        return s;
+        String ans = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
+                ans += s.charAt(i);
+            }
+            else if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
+                int a = s.charAt(i) + 32;
+                char ch = (char) a;
+                ans += ch;
+            }
+            else{
+                ans += s.charAt(i);
+            }
+        }
+        return ans;
     }
 }
