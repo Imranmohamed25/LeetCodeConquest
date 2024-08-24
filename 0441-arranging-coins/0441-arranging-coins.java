57f8a4c7519d448a4a20a2ce6a0c6d0ee5c6842b
Imranmohamed M <123489831+Imranmohamed25@users.noreply.github.com>
2024-08-25 00:16:38 +0530

    Time: 8 ms (30.34%), Space: 41.1 MB (27.69%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int count = 0;
+        int i = 1;
+        while (i <= n) {
+            n = n - i;
+            count++;
+            i++;
+        }
+        return count;
+    }
+}
