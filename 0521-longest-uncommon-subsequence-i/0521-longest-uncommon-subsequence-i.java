27b6adda642f9a57f8885bdca72b50e9ce129429
Imranmohamed M <123489831+Imranmohamed25@users.noreply.github.com>
2024-08-26 00:20:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.java
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.java
@@ -1,8 +1,19 @@
 class Solution {
     public int findLUSlength(String a, String b) {
-        if(a.equals(b)){
+        int count = 0;
+        int a1 = a.length();
+        int b1 = b.length();
+        for (int i = 0; i < a1; i++) {
+            for (int j = 0; j < b1; j++) {
+                if (a.charAt(i) != b.charAt(j)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        if (count == 0 && a1 == b1) {
             return -1;
         }
-        return Math.max(a.length(), b.length());
+        return Math.max(a1, b1);
     }
 }
