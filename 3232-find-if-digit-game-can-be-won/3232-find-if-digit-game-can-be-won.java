e617f27ddcde5bf69d231d8ec256bf15c85d1898
Imranmohamed M <123489831+Imranmohamed25@users.noreply.github.com>
2024-08-30 15:32:10 +0530

    Time: 1 ms (61.73%), Space: 44.1 MB (42.69%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -3,17 +3,11 @@ class Solution {
         int a1 = 0, b2 = 0;
         int b1 = 0, a2 = 0;
         for(int i = 0; i < nums.length; i++){
-            int dig = 0;
-            int temp = nums[i];
-            while(temp > 0){
-                dig++;
-                temp = temp / 10;
-            }
-            if(dig == 2){
+            if(nums[i] < 10){
                 a1 += nums[i];
                 b2 += nums[i];
             }
-            else{
+            else if(nums[i] > 9){
                 b1 += nums[i];
                 a2 += nums[i];
             }
@@ -24,3 +18,9 @@ class Solution {
         return false;
     }
 }
+            // int dig = 0;
+            // int temp = nums[i];
+            // while(temp > 0){
+            //     dig++;
+            //     temp = temp / 10;
+            // }
